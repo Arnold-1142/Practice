fa43d36c960bd4d4ae8fd6a0b8af7cd108b13e9e
3-126-3-PC <you@example.com>
2025-05-17 12:56:39 +1200

    ОБЯЗАТЕЛЬНО КОММЕНТ

--- Practice/Работа 1/js/script.js
+++ Practice/Работа 1/js/script.js
@@ -2,30 +2,51 @@ const HOST = 'http://api-messenger.web-srv.local';
 var TOKEN = '';
 const CONTENT = document.querySelector('.content');
 
-loadPageAuth();
-function loadPageAuth(){
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "/modules/entrance.html");
-    xhr.send();
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState == 4){
-            CONTENT.innerHTML = xhr.responseText
-            onLoadPageAuth()
+
+function _get(params, callback) {
+    var HTTP_REQUEST = new XMLHttpRequest();
+    HTTP_REQUEST.open('GET', `${params.url}`);
+    HTTP_REQUEST.send();
+    HTTP_REQUEST.onreadystatechange = function() {
+        if(HTTP_REQUEST.readyState == 4) {
+            callback(HTTP_REQUEST.responseText);
         }
     }
 }
 
-oadPageAuth();
-function loadPageAuth(){
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "/modules/registration.html");
-    xhr.send();
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState == 4){
-            CONTENT.innerHTML = xhr.responseText
-            onLoadPageAuth()
+function _post (params, callback) {
+    var HTTP_REQUEST = new XMLHttpRequest();
+    HTTP_REQUEST.open('POST', `${params.url}`);
+    HTTP_REQUEST.send(params.data);
+    HTTP_REQUEST.onreadystatechange = function() {
+        if(HTTP_REQUEST.readyState == 4) {
+            callback(HTTP_REQUEST.responseText);
         }
     }
 }
+
+LoadPageAuth()
+
+
+function LoadPageAuth () {
+    _get ({url: '/modules/entrance.html'}, function(responseText) {
+        CONTENT.innerHTML = responseText
+    })
+}
+
+onLoadPageAuth()
+
+
+function onLoadPageAuth() {
+    document.querySelector('.entrance').addEventListener('click', function() {
+        var request_data = new FormData();
+        request_data.append('?', document.querySelector('input[name="?"]').value)
+        request_data.append('?', document.querySelector('input[name="?"]').value)
+        request_data.append('?', document.querySelector('input[name="?"]').value)
+        request_data.append('?', document.querySelector('input[name="?"]').value)
+    })
+}
+
+
 
 
