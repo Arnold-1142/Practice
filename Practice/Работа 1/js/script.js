e0def6f0cdfad43c20007b2f35d1c75724ceab14
3-126-3-PC <you@example.com>
2025-05-20 12:43:54 +1200

    ОБЯЗАТЕЛЬНО КОММЕНТ

--- Practice/Работа 1/js/script.js
+++ Practice/Работа 1/js/script.js
@@ -47,6 +47,107 @@ function onLoadPageAuth() {
     })
 }
 
+//АВТОРИЗАЦИЯ
+ _get({ url: '/modules/entrance.html' }, function (responseText) {
+    CONTENT.innerHTML = responseText;
+    _elem('.go-register').addEventListener('click', function () {
+        _load('/modules/registration.html', function (responseText) {
+            CONTENT.innerHTML = responseText;
+
+            _elem('.register').addEventListener('click', function () {
 
 
+                let rdata = new FormData()
+
+
+                rdata.append('first_name', _elem('input[name="first_name"]').value)
+                rdata.append('last_name', _elem('input[name="last_name"]').value)
+                rdata.append('email', _elem('input[name="email"]').value)
+                rdata.append('password', _elem('input[name="password"]').value)
+
+                _post({ url: `${HOST}/registration`, data: rdata }, function (responseText) {
+                    responseText = JSON.parse(responseText)
+                    console.log(responseText);
+                    if (responseText.success) {
+                        token = responseText.token
+                        console.log(token)
+                        _load('/modules/profile.html', function (responseText) {
+                            CONTENT.innerHTML = responseText
+                        })
+                    }
+
+                })
+            })
+        })
+
+    })
+
+ 
+
+
+
+  _elem('.chat').addEventListener('click', function () {
+        let edata = new FormData()
+        let email = _elem('input[name="email"]').value
+        let password = _elem('input[name="password"]').value
+        edata.append('email', email)
+        edata.append('password', password)
+  _post({ url: `${HOST}/chat`, data: edata }, function (responseText) {
+            responseText = JSON.parse(responseText)
+            console.log(responseText);
+            if (responseText.success) {
+                token = responseText.token
+                console.log(token)
+  _load('/modules/profile.html', function (responseText) {
+                CONTENT.innerHTML = responseText
+
+  _elem('.btn-upload-file').addEventListener('click', function () {
+  _get({ url: `/modules/upload.html` }, function (responseText) {
+            CONTENT.innerHTML = responseText
+
+  _elem('.upload-files').addEventListener('click', function () {
+
+            })
+
+  _elem('.btn-to-disk').addEventListener('click', function () {
+  _get({ url: `/modules/profile.html` }, function (responseText) {
+            CONTENT.innerHTML = responseText
+                                })
+                            })
+                        })
+                    })
+
+                })
+            }
+            else {
+                alert("login failed")
+            }
+
+        })
+
+    })
+
+
+ })
+
+
+_get({ url: `/modules/logout` }, function(responseText){
+    CONTENT.innerHTML = responseText
+ })
+
+
+_post({ url: `${HOST}/upload`, data: rdata }, function (responseText){
+   responseText = JSON.parse(responseText)
+})
+
+const path = require('node: path');
+path.basename('C:\\temp\\profile.html')
+
+
+// _delete(modules/upload.html)
+
+
+_get({url:`${HOST}/disk`, data: adata}, function (responseText){
+    filesTable = JSON.parse(responseText)
+})
 
